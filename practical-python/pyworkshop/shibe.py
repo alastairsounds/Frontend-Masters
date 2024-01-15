8891798301272a2b9aa32a49a3c19cfcb0dea606
“Alastair <66482739+alastairsounds@users.noreply.github.com>
2024-01-15 16:06:59 -0700

    requests Library: shibe multiple requests

--- practical-python/pyworkshop/shibe.py
+++ practical-python/pyworkshop/shibe.py
@@ -3,23 +3,20 @@
 # First thing we'll do is import the requests library
 import requests
 
-# Define a variable with the URL of the API
-api_url = "http://shibe.online/api/shibes?count=1"
+# We'll store our base URL here and pass in the count parameter later
+api_url = "http://shibe.online/api/shibes"
 
-# Call the root of the api with GET, store the answer in a response variable
-# This call will return a list of URLs that represent dog pictures
-response = requests.get(api_url)
+params = {
+   "count": 10
+}
 
-# Get the status code for the response. Should be 200 OK
-# Which means everything worked as expected
-print(f"Response status code is: {response.status_code}")
+# Pass those params in with the request.
+api_response = requests.get(api_url, params=params)
 
-# Get the result as JSON
-response_json = response.json()
+print(f"Shibe API Response Status Code is: {api_response.status_code}")  # should be 200 OK
 
-# Print it. We should see a list with one image URL.
-for idx, item in enumerate(response_json):
-    print(f"Item {idx}: {item}")
-# # Simple print for small lists
-# print(response_json)
+json_data = api_response.json()
 
+print("Here is a list of URLs for dog pictures:")
+for url in json_data:
+    print(f"\t {url}")
