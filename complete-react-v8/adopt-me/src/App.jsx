4266db1fb94e306aafcea1266762d0227757815f
“Alastair <66482739+alastairsounds@users.noreply.github.com>
2024-01-18 12:12:32 -0700

    Add @tanstack/react-query package and QueryClientProvider

--- complete-react-v8/adopt-me/src/App.jsx
+++ complete-react-v8/adopt-me/src/App.jsx
@@ -1,18 +1,32 @@
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
 const App = () => {
   return (
+    // BrowserRouter and QueryClientProvider are higher-order components
+    // They only provide context to their children. They do not display anything
     <BrowserRouter>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
